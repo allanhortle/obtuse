9bf8af2eb2dcd083822276a4003983b94c42d1dd
Allan Hortle <allanhortle@gmail.com>
2017-05-31 10:26:57 +1000

    Add deadend

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 
 import goose from 'goose-css';
 import {default as Text} from './component/Text';
+import {default as DeadEnd} from './component/DeadEnd';
 
 module.exports = {
     ...goose,
+    DeadEnd,
     Text
 };
